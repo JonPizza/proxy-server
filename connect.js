02d455fb31912fe47075fbf69f096d31ae2a5451
Jon <45501389+JonPizza@users.noreply.github.com>
2019-05-15 08:24:25 -0400

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,9 +1,27 @@
 url = document.getElementById('url');
 connect = document.getElementById('connect');
 click = document.getElementById('click');
+frame = document.getElementById('frame_a');
 
 click.onclick = updateUrl; 
+connect.onclick = loadHTML;
 
 function updateUrl() {
-    connect.setAttribute('href', url.value);
+    connect.innerHTML('Connect to: ' + url.value);
 }
+//------- API
+function loadHTML() {
+    var request = new XMLHttpRequest()
+    request.open('GET', url.value, true)
+    request.onload = function() {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            frame.innerHTML = data;
+        } else {
+            console.log('bu hao')
+        }
+    }
+    request.send()
+}
+
